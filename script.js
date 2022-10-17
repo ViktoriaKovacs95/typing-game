cf11bdf5740c312ea9acc2066cb1cbd6ca1fa345
Viktoria Kovacs <viktoriakovacs@Viktorias-iMac.attlocal.net>
2022-10-17 18:26:02 -0500

    started assignment

---
+++ script.js
@@ -0,0 +1,21 @@
+const quote = document.getElementById('quote');
+const input = document.getElementById('typed-value');
+const start = document.getElementById('start');
+let targetWord;
+function startGame() {
+    console.log("Game started!");
+
+targetWord = "typeme";
+quote.innerHTML = `<span>${targetWord}</span>`;
+}
+start.addEventListener('click', startGame);
+input.addEventListener('input', checkInput);
+
+function checkInput() {
+    const currentWord = targetWord;
+    const typedValue = input.value.trim();
+    if (currentWord !== typedValue) {
+        input.className = currentWord.startsWith(typedValue) ? "" : "error";
+        return;
+    }
+}
